3e928523139c216d38f61aa54a8456604b791137
WOODY <wenyang.hu1119@gmail.com>
2020-05-06 22:36:16 +0800

    final

--- r.py
+++ r.py
@@ -1,12 +1,20 @@
 import random
-r = random.randint(1, 100)
+start = input('請決定開始值')
+end = input('請決定結束')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
+count = 0
 while True:
+    count += 1
     num = input('請猜數字: ')
     num = int(num)
     if num == r:
         print('你猜中了')
+        print('這是你猜的第', count, '次')
         break
     elif num > r:
         print('比答案大')
     elif num < r:
         print('比答案小')
+    print('這是你猜的第', count, '次')
